626610e915fe1f6d9662316e1e497c344654df69
Sonja Madetoja <sonja.madetoja@helsinki.fi>
2024-08-01 12:22:21 +0300

    osa 4 sovelluksen rakenne, testit, async/await

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
@@ -8,32 +8,32 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://sonjamadetoja:${password}@cluster0.frewxha.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://sonjamadetoja:${password}@cluster0.frewxha.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-// const note = new Note({
-//   content: 'Mongoose makes things easy',
-//   important: true,
-// })
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   console.log('result: ', result.content)
-//   mongoose.connection.close()
-// })
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
   })
-  mongoose.connection.close()
+
+  const Note = mongoose.model('Note', noteSchema)
+
+
+  const note = new Note({
+    content: 'test note 3',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
 })
